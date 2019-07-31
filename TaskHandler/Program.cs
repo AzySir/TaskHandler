8bfc27c969d4c012d1c6b03b5eebb863a5507f6b
AzySir <azy.sir@gmail.com>
2019-07-31 11:48:48 +1000

    First Commit

---
+++ TaskHandler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TaskHandler
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DateTime Now = DateTime.UtcNow;
+            DateTime CurrentTimeZoneNow = TimeZone.CurrentTimeZone.ToLocalTime(Now);
+            
+            //Create User
+            User u = new User("Adam Sir", 111);
+            //Read User
+            Console.WriteLine("User Name: " + u.Name);
+
+            u.AddTask("Cook Breakfast");
+
+            //Create Task
+            u.AddTask("Cook Breakfast", CurrentTimeZoneNow);
+
+            //Start Task
+            u.StartTask(CurrentTimeZoneNow);
+
+            //Get TaskStartTime()
+            Console.WriteLine("Current Start Time: " + u.GetStartTime().ToString("hh:mm tt"));
+
+            //End Task
+
+            //Calculate Task
+        }
+    }
+}
---
+++ TaskHandler/Task.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskHandler
+{
+    public class Task
+    {
+        private String TaskName;
+        private int TaskId;
+        private DateTime StartTime;
+        private DateTime EndTime;
+        private String TaskStatus;
+        //private Dictionary<String, int> dic = new Dictionary<String, int>();
+        private List<Task> MyTasks = new List<Task>();
+
+        public void AddTask(String TaskName)
+        {
+            //Auto Generate the ID
+            this.TaskName = TaskName; //Add Task Name
+            //StartTime is Null
+            //EndTime is Null
+            //Task Status
+        }
+        public void AddTask(String TaskName, DateTime StartTime, String TaskStatus) { 
+            //Auto Generate the ID
+            this.TaskName = TaskName; //Add Task Name
+            this.StartTime = StartTime; //Add StartTime
+            this.TaskStatus = TaskStatus; //Add Task Status
+        }
+        public void AddTask(String TaskName, DateTime StartTime)
+        {
+            this.TaskName = TaskName;
+            this.StartTime = StartTime;
+
+            if (StartTime < DateTime.Now)
+            {
+                this.TaskStatus = "Not Started";
+            }
+            else
+            {
+                this.TaskStatus = "Started";
+            }
+        }
+
+        #region ----------Extra Tasks---------
+
+        public String GetTask()
+        {
+            return this.TaskName;
+        }
+
+        public void DeleteTask()
+        {
+
+        }
+
+        public void StartTask(DateTime StartTime)
+        {
+            this.StartTime = StartTime;
+        }
+
+        public void EndTask(DateTime EndTime)
+        {
+            this.EndTime = EndTime;
+        }
+
+        public DateTime GetStartTime()
+        {
+            return this.StartTime;
+        }
+
+        public DateTime GetEndTime()
+        {
+            return this.EndTime;
+        }
+
+        public void SetResumeType()
+        {
+            //Options
+                //a) Manual - User Entry to Begin Task
+                //b) Automatic Start - Depending on Time
+        }
+
+        #endregion
+    }
+}
